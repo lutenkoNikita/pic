f12b4d669491131f405615deeb4893a0dace426a
lutenkoNikita <90026027+lutenkoNikita@users.noreply.github.com>
2022-11-17 17:36:36 +0200

    Create CatalyticConverter.java

---
+++ CatalyticConverter.java
@@ -0,0 +1,15 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CatalyticConverter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CatalyticConverter.class);
+
+    public void on() {
+        LOGGER.info("Catalytic Converter switched on!");
+    }
+
+    public void off() {
+        LOGGER.info("Catalytic Converter switched off!");
+    }
+}

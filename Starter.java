61acc07c3896d0982e1f1990fff4206fdcacc19c
lutenkoNikita <90026027+lutenkoNikita@users.noreply.github.com>
2022-11-17 17:38:56 +0200

    Create Starter.java

---
+++ Starter.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Starter.class);
+
+    public void start() {
+        LOGGER.info("Starting..");
+    }
+}
